d24fa602c475e57fbfb1146dc62c24de9e1e1ab3
Patrick Fonseca <patrick.fcf@gmail.com>
2018-04-04 12:01:48 +0100

    Added "in construction" initial page and beta test page for devs
    Added "in construction" initial page and beta test page for devs

--- index.php
+++ index.php
@@ -13,66 +13,7 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-info">
-
-      <div class="container">
-
-        <a class="navbar-brand h1 mb-0" href="#">UniRent</a>  
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSite">
-          
-          <ul class="navbar-nav mr-auto">
-            
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ínicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Como Funciona</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Nosso Time</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Depoimentos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contactos</a>
-            </li>
-          
-          </ul>
-
-          <ul class="navbar-nav ml-auto">
-            
-            <li class="nav-item dropdown">
-
-              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="navDrop">
-                Social
-              </a>
-
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Facebook</a>
-                <a class="dropdown-item" href="#">Twitter</a>
-                <a class="dropdown-item" href="#">Instagram</a>
-              </div>
-
-            </li>
-
-          </ul>
-
-          <form class="form-inline">
-            <input class="form-control ml-4 mr-2" type="search" placeholder="Buscar...">
-            <button class="btn btn-dark" type="Submit">OK</button>
-          </form>
-        
-        </div>
-
-      </div>
     
-    </nav>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
